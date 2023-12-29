ced01e9d68ecf94556afabb0d8b88f2529d07c8f
mohan.poojary <mohan.poojary@manipal.net>
2023-12-29 07:38:13 +0530

    commit

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,31 +1,36 @@
-
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
+
 const App = () => {
-  const[todo, setTodo] = useState([]);
+  const initialTodo = JSON.parse(localStorage.getItem('todo')) || [];
+  const [todo, setTodo] = useState(initialTodo);
+
+  useEffect(() => {
+    localStorage.setItem('todo', JSON.stringify(todo));
+  }, [todo]);
 
   const addTodo = (name) => {
     const newTodo = {
-      id:new Date().getTime(),
-      name:name,
+      id: new Date().getTime(),
+      name: name,
     };
-    setTodo((prevTodo)=> [...prevTodo, newTodo]);
+    setTodo((prevTodo) => [...prevTodo, newTodo]);
   };
+
   return (
     <div className=''>
-      <Header/>
-      <Form addTodo={addTodo}/>
+      <Header />
+      <Form addTodo={addTodo} />
       <div>
         <h2>Entered Information</h2>
         <ul>
-          {todo.map((todos)=>(
+          {todo.map((todos) => (
             <li key={todos.id}>{todos.name}</li>
           ))}
         </ul>
       </div>
     </div>
-    
   );
 }
 
